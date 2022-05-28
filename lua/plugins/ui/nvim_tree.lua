85355d2f65207ed29c0fa6d1cd9c4c1f936cf464
Axel Vasquez <a.axelvc@gmail.com>
2022-05-28 01:27:43 -0500

    refactor: update nvim-tree config

--- lua/plugins/ui/nvim_tree.lua
+++ lua/plugins/ui/nvim_tree.lua
@@ -1,51 +1,17 @@
 local nvim_tree = require 'nvim-tree'
+local autocmd = vim.api.nvim_create_autocmd
 local map = vim.keymap.set
 local g = vim.g
 
-vim.g.nvim_tree_highlight_opened_files = 1
-vim.g.nvim_tree_add_trailing = 0
-vim.g.nvim_tree_group_empty = 1
-vim.g.nvim_tree_refresh_wait = 100
-vim.g.nvim_tree_show_icons = {
-  git = 1,
-  folders = 1,
-  files = 1,
-  folder_arrows = 0,
-}
-vim.g.nvim_tree_icons = {
-  default = '',
-  git = {
-    deleted = 'D',
-    ignored = 'I',
-    renamed = 'R',
-    staged = 'A',
-    unmerged = '',
-    unstaged = 'M',
-    untracked = 'U',
-  },
-}
-
 nvim_tree.setup {
-  disable_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {},
-  auto_reload_on_write = true,
-  open_on_tab = false,
-  update_cwd = false,
-  hijack_netrw = true,
-  hijack_cursor = true,
-  hijack_unnamed_buffer_when_opening = true,
-  hijack_directories = {
-    enable = true,
-    auto_open = true,
-  },
-  renderer = {
-    indent_markers = {
-      enable = true,
-    },
+  view = {
+    width = 45,
+    side = 'right',
+    hide_root_folder = true,
   },
   diagnostics = {
-    enable = false,
+    enable = true,
+    show_on_dirs = false,
     icons = {
       hint = g.signs.hint,
       info = g.signs.info,
@@ -53,33 +19,59 @@ nvim_tree.setup {
       error = g.signs.error,
     },
   },
+  renderer = {
+    group_empty = true,
+    add_trailing = false,
+    highlight_opened_files = 'name',
+    indent_markers = {
+      enable = true,
+    },
+    icons = {
+      webdev_colors = true,
+      git_placement = 'after',
+      padding = ' ',
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = false,
+        git = true,
+      },
+      glyphs = {
+        default = '',
+        symlink = '',
+        folder = {
+          arrow_closed = '',
+          arrow_open = '',
+          default = '',
+          open = '',
+          empty = '',
+          empty_open = '',
+          symlink = '',
+          symlink_open = '',
+        },
+        git = {
+          deleted = 'D',
+          ignored = 'I',
+          renamed = 'R',
+          staged = 'A',
+          unmerged = '',
+          unstaged = 'M',
+          untracked = 'U',
+        },
+      },
+    },
+  },
   update_focused_file = {
     enable = true,
-    update_cwd = true,
-    ignore_list = {},
+    update_cwd = false,
   },
   system_open = {
-    cmd = nil,
-    args = {},
+    cmd = 'wslview',
   },
   filters = {
     dotfiles = false,
     custom = { '.git' },
-  },
-  git = {
-    enable = true,
-    ignore = false,
-  },
-  view = {
-    width = 45,
-    hide_root_folder = true,
-    side = 'right',
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = '?', cb = ':WhichKey<CR>' },
-      },
-    },
+    exclude = {},
   },
   actions = {
     change_dir = {
@@ -97,3 +89,13 @@ nvim_tree.setup {
 map('n', '<C-n>', function()
   nvim_tree.toggle(true)
 end)
+
+-- Close vim if the last buf is NvimTree
+autocmd('BufEnter', {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match 'NvimTree_' ~= nil then
+      vim.cmd 'quit'
+    end
+  end,
+})
