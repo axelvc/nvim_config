8db6f35e40ba09b24b89096fdcf9b5ea228ad17d
Axel Vasquez <a.axelvc@gmail.com>
2022-05-28 22:52:22 -0500

    update Comment plugin config

--- lua/plugins/utils/comment.lua
+++ lua/plugins/utils/comment.lua
@@ -2,8 +2,49 @@ local map = vim.keymap.set
 
 require('Comment').setup {
   ignore = '^$',
-  pre_hook = function()
-    return require('ts_context_commentstring.internal').calculate_commentstring()
+  toggler = {
+    line = 'gcc',
+    block = 'gcb',
+  },
+  opleader = {
+    line = 'gc',
+    block = 'gb',
+  },
+  extra = {
+    above = 'gcO',
+    below = 'gco',
+    eol = 'gcA',
+  },
+  mappings = {
+    basic = true,
+    extra = true,
+    extended = false,
+  },
+  pre_hook = function(ctx)
+    local to_calculate = {
+      typescriptreact = true,
+      javascript = true
+    }
+
+    if to_calculate[vim.opt.filetype:get()] then
+      local U = require 'Comment.utils'
+
+      -- Determine whether to use linewise or blockwise commentstring
+      local type = ctx.ctype == U.ctype.line and '__default' or '__multiline'
+
+      -- Determine the location where to calculate commentstring from
+      local location = nil
+      if ctx.ctype == U.ctype.block then
+        location = require('ts_context_commentstring.utils').get_cursor_location()
+      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+        location = require('ts_context_commentstring.utils').get_visual_start_location()
+      end
+
+      return require('ts_context_commentstring.internal').calculate_commentstring {
+        key = type,
+        location = location,
+      }
+    end
   end,
 }
 
